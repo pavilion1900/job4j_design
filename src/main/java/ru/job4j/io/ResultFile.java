ab121a711b74703057cec3247da122d6504c0844
pavilion1900 <pavilion1900@gmail.com>
2021-04-17 22:47:00 +0300

    0.1. FileOutputStream. [#477564]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            int[][] table = Matrix.multiple(9);
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table.length; j++) {
+                    out.write(String.format("%4d", table[i][j]).getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
