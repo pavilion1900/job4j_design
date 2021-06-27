1e5c73c2a3ac815be615b961abc8e212504d840a
pavilion1900 <pavilion1900@gmail.com>
2021-06-27 04:51:55 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (!line.startsWith("#") && !line.isEmpty()) {
+                    String[] array = line.split("=");
+                    if (array.length == 1) {
+                        throw new IllegalArgumentException();
+                    }
+                    values.put(array[0], array[1]);
+                }
+            }
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        if (key == null) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenCommentAndEmptyLine() {
+        String path = "app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenException() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+}
