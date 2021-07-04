9d5c780bd9e7966804d6f4245b80a416fd99532d
pavilion1900 <pavilion1900@gmail.com>
2021-07-05 00:41:08 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -8,6 +10,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void whenServer1() {
@@ -46,5 +51,47 @@ public class AnalizyTest {
             e.printStackTrace();
         }
         assertThat(rsl, is(exp));
+    }
+
+    @Test
+    public void whenServer1TempFolder() throws IOException {
+        Analizy analizy = new Analizy();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator()
+                    + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator()
+                    + "300 11:02:02");
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;" + "11:01:02;11:02:02;"));
+    }
+
+    @Test
+    public void whenServer2TempFolder() throws IOException {
+        Analizy analizy = new Analizy();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator()
+                    + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "500 10:59:01" + System.lineSeparator()
+                    + "400 11:01:02" + System.lineSeparator()
+                    + "200 11:02:02");
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;11:02:02;"));
     }
 }
