ee8fe455c2d832fe44442267a014180eb55d279c
pavilion1900 <pavilion1900@gmail.com>
2021-07-11 19:15:30 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,47 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+    private Predicate<Path> predicate;
+    private List<Path> paths = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> predicate) {
+        this.predicate = predicate;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+            throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            throws IOException {
+        if (predicate.test(file)) {
+            paths.add(file.getFileName());
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+}
