5f3ae0018dc9618d7e538ea4290aa89a611544db
pavilion1900 <pavilion1900@gmail.com>
2021-10-10 22:46:57 +0300

    some changes

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -13,9 +13,7 @@ public class Dir {
             throw new IllegalArgumentException(
                     String.format("Not directory %s", file.getAbsoluteFile()));
         }
-//        System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-//            System.out.println(subfile.getAbsoluteFile());
             System.out.println(subfile.getName() + " " + subfile.length() + " bytes");
         }
     }
