2e2dc35eb2dddb7307326885a3c6ea767587860c
pavilion1900 <pavilion1900@gmail.com>
2022-01-16 23:49:34 +0300

    0.1. Statement [#379306 #251785]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -14,47 +14,47 @@ public class TableEditor implements AutoCloseable {
     }
 
     private void initConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String url = this.properties.getProperty("hibernate.connection.url");
-        String login = this.properties.getProperty("hibernate.connection.username");
-        String password = this.properties.getProperty("hibernate.connection.password");
+        final String url = this.properties.getProperty("hibernate.connection.url");
+        final String login = this.properties.getProperty("hibernate.connection.username");
+        final String password = this.properties.getProperty("hibernate.connection.password");
+        final String driver = this.properties.getProperty("hibernate.connection.driver_class");
+        Class.forName(driver);
         connection = DriverManager.getConnection(url, login, password);
     }
 
     private void executeStatement(String sql, String tableName) throws Exception {
-        try (Connection connection = this.connection) {
-            try (Statement statement = connection.createStatement()) {
-                statement.execute(sql);
-                if (!sql.contains("drop table")) {
-                    System.out.println(getTableScheme(connection, tableName));
-                }
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            if (!sql.contains("drop table")) {
+                System.out.println(getTableScheme(connection, tableName));
             }
+            connection.close();
         }
     }
 
     public void createTable(String tableName) throws Exception {
-        String sql = String.format("create table if not exists %s ();", tableName);
+        final String sql = String.format("create table if not exists %s ();", tableName);
         executeStatement(sql, tableName);
     }
 
     public void dropTable(String tableName) throws Exception {
-        String sql = String.format("drop table %s;", tableName);
+        final String sql = String.format("drop table %s;", tableName);
         executeStatement(sql, tableName);
     }
 
     public void addColumn(String tableName, String columnName, String type) throws Exception {
-        String sql = String.format("alter table %s add %s %s;", tableName, columnName, type);
+        final String sql = String.format("alter table %s add %s %s;", tableName, columnName, type);
         executeStatement(sql, tableName);
     }
 
     public void dropColumn(String tableName, String columnName) throws Exception {
-        String sql = String.format("alter table %s drop column %s;", tableName, columnName);
+        final String sql = String.format("alter table %s drop column %s;", tableName, columnName);
         executeStatement(sql, tableName);
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName)
             throws Exception {
-        String sql = String.format("alter table %s rename column %s to %s;",
+        final String sql = String.format("alter table %s rename column %s to %s;",
                 tableName, columnName, newColumnName);
         executeStatement(sql, tableName);
     }
@@ -87,7 +87,11 @@ public class TableEditor implements AutoCloseable {
 
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        properties.load(new FileReader("app.properties"));
+        try (BufferedReader in = new BufferedReader(new FileReader("app.properties"))) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         TableEditor editor = new TableEditor(properties);
         String tableName = "demo_table2";
         String columnName = "email";
