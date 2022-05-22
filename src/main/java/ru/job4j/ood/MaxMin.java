7d80484caeed0c75b1bfcee806daec52cec7f2df
Korolenko Sergey <pavilion1900@gmail.com>
2022-05-22 23:05:34 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #303636]1

--- src/main/java/ru/job4j/ood/MaxMin.java
+++ src/main/java/ru/job4j/ood/MaxMin.java
@@ -2,21 +2,21 @@ package ru.job4j.ood;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return findValue(value, element -> comparator.compare(value.get(0), element) < 0);
+        return findValue(value, (result, element) -> comparator.compare(result, element) < 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return findValue(value, element -> comparator.compare(value.get(0), element) > 0);
+        return findValue(value, (result, element) -> comparator.compare(result, element) > 0);
     }
 
-    public <T> T findValue(List<T> value, Predicate<T> predicate) {
+    public <T> T findValue(List<T> value, BiPredicate<T, T> predicate) {
         T result = value.get(0);
         for (T element : value) {
-            if (predicate.test(element)) {
+            if (predicate.test(result, element)) {
                 result = element;
             }
         }
