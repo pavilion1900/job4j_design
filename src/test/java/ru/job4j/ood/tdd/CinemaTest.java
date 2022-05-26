9f57e0d553e9a7f9f7cccb91d260af506e4466a5
Korolenko Sergey <pavilion1900@gmail.com>
2022-05-26 20:53:42 +0300

    2. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/ood/tdd/CinemaTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.ood.tdd;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+@Ignore
+public class CinemaTest {
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void whenDontBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket1, is(new Ticket3D()));
+        assertNull(ticket2);
+    }
+
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void whenDontFind() {
+        Cinema cinema = new Cinema3D();
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions.size(), is(0));
+    }
+
+    @Test
+    public void whenAddOneSession() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions.size(), is(1));
+        assertTrue(sessions.contains(new Session3D()));
+    }
+}
